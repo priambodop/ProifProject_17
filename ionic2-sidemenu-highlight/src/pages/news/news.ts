02e28bbd3c8fab8d01323c077fe6bce178e55ee3
Siti khalishah <siti.khalishah@yahoo.com>
2017-03-02 23:11:46 +0700

    loader in news page

--- ionic2-sidemenu-highlight/src/pages/news/news.ts
+++ ionic2-sidemenu-highlight/src/pages/news/news.ts
@@ -9,9 +9,11 @@ import { RugbyService } from '../../app/services/rugby.service';
 export class News {
 
   items : [any];
+  public status: string;
+
 
   constructor(public navCtrl: NavController, private rugbyService: RugbyService) {
-
+      this.status = 'loading';
   }
 
   ngOnInit(){
@@ -21,6 +23,7 @@ export class News {
   getPosts(){
     this.rugbyService.getPosts().subscribe(response => {
       this.items = response;
+      this.status = 'active';
     });
   }
 
