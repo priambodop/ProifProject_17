b5226b05deff35d8d14bbbf616cc5fb6851e36ca
kekresna <alvinkresna@gmail.com>
2017-03-07 11:37:52 +0700

    + Camera, blm sync ke JSON

--- ionic2-sidemenu-highlight/src/pages/betaTested/betaTested.ts
+++ ionic2-sidemenu-highlight/src/pages/betaTested/betaTested.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Photo } from '../photo';
 import {RugbyService} from '../../app/services/rugby.service';
-
+import { Camera } from 'ionic-native';
 @Component({
   selector: 'betaTested',
   templateUrl: 'betaTested.html'
@@ -15,4 +15,16 @@ export class BetaTested {
 
   photos: Photo[] = [new Photo('http://placehold.it/350/150',5), new Photo('http://placehold.it/350/151',6)];
 
+  takePhoto(){
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      targetHeight: 500,
+      targetWidth: 500,
+      correctOrientation: true
+    }).then((imageData)=> {
+      this.photos.push(new Photo('data:image/jpeg;base64,' + imageData,0));
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
