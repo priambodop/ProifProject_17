cf306495e209fc7f9a40a2df2bb6e6614ec1ca89
Siti khalishah <siti.khalishah@yahoo.com>
2017-03-07 10:25:53 +0700

    header

--- ionic2-sidemenu-highlight/src/app/app.component.ts
+++ ionic2-sidemenu-highlight/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
-//import { Page1 } from '../pages/page1/page1';
-//import { Page2 } from '../pages/page2/page2';
 import { RugbyService } from './services/rugby.service';
 import { News } from '../pages/news/news';
 import { Fixtures } from '../pages/fixtures/fixtures';
