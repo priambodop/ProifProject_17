2fc44b18f0311cc6350dc1afb77edb433a69178a
priambodop <priambodop>
2017-03-07 12:58:33 +0700

    up overalll

--- ionic2-sidemenu-highlight/src/pages/teammate/teammate.ts
+++ ionic2-sidemenu-highlight/src/pages/teammate/teammate.ts
@@ -2,34 +2,31 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {RugbyService} from '../../app/services/rugby.service';
 import { AlertController } from 'ionic-angular';
+import {Camera} from 'ionic-native';
+import {Photo} from '../photo';
 
 @Component({
   selector: 'teammate',
   templateUrl: 'teammate.html'
 })
 export class Teammate {
-  items: [any];
+  public base64Image: string;
+  photos: Photo[] = [new Photo('http://placehold.it/350/150')]
 
-  constructor(public navCtrl: NavController, public alerCtrl: AlertController, private rugbyService: RugbyService) {
+  constructor(public navCtrl: NavController, private rugbyService: RugbyService) {
+
   }
 
-  ngOnInit(){
-    this.getTeammates();
-  }
-
-  uploadAlert(){
-    let alert = this.alerCtrl.create({
-      title: 'Alert',
-      message: 'Coming Soon!',
-      buttons: ['Ok']
-    });
-    alert.present()
-  }
-
-
-  getTeammates(){
-    this.rugbyService.getTeammates().subscribe(response => {
-      this.items = response.data;
+  takePhoto(){
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      targetHeight:500,
+      targetWidth:500,
+      correctOrientation:true
+    }).then((imageData) => {
+      this.photos.push(new Photo('data:image/jpeg;base64,' + imageData));
+    }, (err) =>{
+      console.log(err);
     });
   }
 }
